d046a70c2c7e40b8cc2bae2047bc2ae6116d88a1
anna_goncharova <anna@bandit-ltd.com>
2015-12-29 23:28:28 +0200

    New version of project with moved test configuration into parent classes

--- src/test/java/ua/com/anya/TodoMVCTest/TodoMVCTest.java
+++ src/test/java/ua/com/anya/TodoMVCTest/TodoMVCTest.java
@@ -1,9 +1,12 @@
 package ua.com.anya.TodoMVCTest;
-import com.codeborne.selenide.Condition;
+
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import ru.yandex.qatools.allure.annotations.*;
 
 
 import static com.codeborne.selenide.CollectionCondition.empty;
@@ -11,87 +14,156 @@ import static com.codeborne.selenide.CollectionCondition.exactTexts;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TodoMVCTest {
+
+public class TodoMVCTest extends AtTodoMVCPageWithClearedDataAfterEachTest {
+
+    @Test
+    public void testDeleteWhileEditing(){
+        add("1", "2");
+        startEdit("1", "").pressEnter();
+        assertExistingTasks("2");
+        assertItemsLeft(1);
+    }
+
+    @Test
+    public void testCancelEditingByESC(){
+        add("1");
+        startEdit("1", "1-edited").sendKeys(Keys.ESCAPE);
+        assertExistingTasks("1");
+        assertItemsLeft(1);
+    }
+
+    @Test
+    public void testEditAndSaveByClickingOutside(){
+        add("1");
+        startEdit("1", "1-edited");
+        add("2");
+        assertExistingTasks("1-edited", "2");
+        assertItemsLeft(2);
+    }
+
+    @Test
+    public void testActivateAllOnCompletedFilter(){
+        // given on completed filter with all tasks completed
+        add("1", "2");
+        toggleAll();
+        openCompletedFilter();
+        assertVisibleTasks("1", "2");
+        assertItemsLeft(0);
+
+        // activate All
+        toggleAll();
+        assertVisibleTasksListIsEmpty();
+        assertItemsLeft(2);
+        openActiveFilter();
+        assertVisibleTasks("1", "2");
+    }
 
     @Test
     public void testTasksMainFlow(){
-
-        open("https://todomvc4tasj.herokuapp.com/");
-        addTasks("1");
-        startEdit("1", "1-edited").pressEnter();
-        toggleTask("1-edited");
-        tasksList.filterBy(cssClass("completed")).shouldHaveSize(1);
+        add("1");
+        toggle("1");
 
         openActiveFilter();
-        addTasks("2");
-        assertExistingTasks("", "2");
-        toggleAllTasks();
+        assertVisibleTasksListIsEmpty();
 
+        add("2");
+        startEdit("2", "2-edited").pressEnter();
+        assertVisibleTasks("2-edited");
+
+        //complete All
+        toggleAll();
+        assertVisibleTasksListIsEmpty();
         openCompletedFilter();
-        assertExistingCompletedTasks("1-edited", "2");
-        toggleTask("1-edited");
-        clearCompletedTasks();
-        tasksList.filter(visible).shouldHaveSize(0);
+        assertVisibleTasks("1", "2-edited");
+
+        //activate
+        toggle("2-edited");
+        assertVisibleTasks("1");
+
+        clearCompleted();
+        assertVisibleTasksListIsEmpty();
 
         openAllFilter();
-        itemsLeft(1);
-        deleteTask("1-edited");
-        tasksList.shouldBe(empty);
+        delete("2-edited");
+        assertExistingTasksListIsEmpty();
     }
 
     ElementsCollection tasksList = $$("#todo-list li");
-    SelenideElement editableField = tasksList.find(cssClass("editing")).find(".edit");
-    SelenideElement newTask = $("#new-todo");
+    ElementsCollection visibleTasks = tasksList.filter(visible);
 
-    private void addTasks(String... tasksTexts){
+    @Step
+    void add(String... tasksTexts){
         for(String taskText: tasksTexts){
-            newTask.setValue(taskText).pressEnter();
+            $("#new-todo").setValue(taskText).pressEnter();
         }
     }
 
+    @Step
+    private void assertVisibleTasks(String... tasksTexts){
+        visibleTasks.shouldHave(exactTexts(tasksTexts));
+    }
+
+    @Step
+    private void assertVisibleTasksListIsEmpty(){
+        visibleTasks.shouldBe(empty);
+    }
+
+    @Step
     private void assertExistingTasks(String... tasksTexts){
         tasksList.shouldHave(exactTexts(tasksTexts));
     }
 
-    private void assertExistingCompletedTasks(String... tasksTexts){
-        $$(".completed label").shouldHave(exactTexts(tasksTexts));
+    @Step
+    private void assertExistingTasksListIsEmpty(){
+        tasksList.shouldBe(empty);
     }
 
+    @Step
     private SelenideElement startEdit(String taskText, String newTaskText){
         tasksList.find(exactText(taskText)).find("label").doubleClick();
-        editableField.setValue(newTaskText);
-        return editableField;
+        return tasksList.find(cssClass("editing")).find(".edit").setValue(newTaskText);
     }
 
-    private void deleteTask(String taskText){
+    @Step
+    private void delete(String taskText){
         tasksList.find(exactText(taskText)).hover().find(".destroy").click();
     }
 
-    private void toggleTask(String taskText){
+    @Step
+    private void toggle(String taskText){
         tasksList.find(exactText(taskText)).find(".toggle").click();
     }
 
-    private void toggleAllTasks(){
+    @Step
+    private void toggleAll(){
         $("#toggle-all").click();
     }
 
-    private void clearCompletedTasks(){
+    @Step
+    private void clearCompleted(){
         $("#clear-completed").click();
+        $("#clear-completed").shouldBe(hidden);
     }
 
-    private void itemsLeft(Integer counter){
-        $("#todo-count strong").shouldHave(exactText(String.valueOf(counter)));
+    @Step
+    private void assertItemsLeft(Integer counter){
+        $("#todo-count>strong").shouldHave(exactText(String.valueOf(counter)));
     }
 
+    @Step
     private void openAllFilter(){
         $(By.linkText("All")).click();
     }
 
+    @Step
     private void openActiveFilter(){
         $(By.linkText("Active")).click();
     }
 
+    @Step
     private void openCompletedFilter(){
         $(By.linkText("Completed")).click();
     }
+
 }
